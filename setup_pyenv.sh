fb07399ff8d9bcf2eb01b6b437e6d811c8804611
Ashish Ranjan Vidyarthi <ashish.vid@gmail.com>
2016-08-02 11:41:37 -0700

    Create setup_pyenv.sh

---
+++ setup_pyenv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p ~/Dotfiles
+#Install pyenv
+
+
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+rc=$?; if [[ $rc != 0 ]]; then echo "error in brew package installation"; exit $rc; fi
+
+# Add to ~/.pyenv_profile and source it.
+
+cat > $HOME/Dotfiles/.pyenv_profile <<- EOM
+export PATH="$HOME/.pyenv/bin:\$PATH"
+eval "\$(pyenv init -)"
+eval "\$(pyenv virtualenv-init -)"
+EOM
+
+
+
+
+
+
+
