1705a92fcf7c9fd7f4d99783faff1aa000e38962
Ashish Ranjan Vidyarthi <ashish.vid@gmail.com>
2017-10-15 02:33:59 -0700

    Add more brew packages

--- brew/tools/setup_lang.sh
+++ brew/tools/setup_lang.sh
@@ -13,5 +13,21 @@ install_brew_package(){
     fi
 }
 
+install_brew_package autoconf
+install_brew_package automake
+install_brew_package bash
+install_brew_package bash-completion
+install_brew_package boost
+install_brew_package boost-build
+install_brew_package cairo
+install_brew_package cmake
+install_brew_package cython
+install_brew_package diff-so-fancy
+install_brew_package tmux
+install_brew_package ruby
+install_brew_package ruby-build
+install_brew_package erlang
+install_brew_package elixir
+install_brew_package graphviz
 install_brew_package golang
 install_brew_package node
