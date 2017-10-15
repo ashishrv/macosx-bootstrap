36c23168953124921202bf7a7af8775a31799f8d
Ashish Ranjan Vidyarthi <ashish.vid@gmail.com>
2017-10-14 21:25:47 -0700

    Add initialization dir

---
+++ basics/initialize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# Setup specific directories
+#
+
+mkdir -p ${HOME}/personal/{workspace,books}	
+mkdir -p ${HOME}/works/{workspace,docs,notes,software}
+mkdir -p ${HOME}/works/software/bin
+mkdir -p ${HOME}/works/workspace/{gitrepo,scratchdir}
+mkdir -p ${HOME}/cloudstorage
+mkdir -p ${HOME}/software
+
+
