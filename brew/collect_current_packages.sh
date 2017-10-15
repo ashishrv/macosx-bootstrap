229bca490d8ab12c545fd651e3317ce672f357e4
Ashish Ranjan Vidyarthi <ashish.vid@gmail.com>
2017-10-14 23:20:51 -0700

    Add scripts to collect packages installed

---
+++ brew/collect_current_packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(cd $(dirname "$0") && pwd -P)
+SCRIPT_FILE=$(basename $0)
+
+fact=`command -v brew`
+if [ "x${fact}" == "x" ]; then
+    echo "Homebrew is not installed."
+else
+    brew list | fmt -1 > $SCRIPT_PATH/brew_packages.txt
+    if brew ls --versions cask > /dev/null; then
+        brew cask list | fmt -1 > $SCRIPT_PATH/cask_packages.txt
+    else
+        echo "Homebrew cask is not installed."
+    fi
+fi
+
+
