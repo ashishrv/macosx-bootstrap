96757154eb4bc29f8229bf938248a00a4826e11c
Ashish Ranjan Vidyarthi <ashish.vid@gmail.com>
2018-10-12 10:36:21 -0700

    refactor setup

--- python/setup_python.sh
+++ python/setup_python.sh
@@ -10,14 +10,16 @@ pyenv_install_pip(){
 	if [ "x${fact}" == "x" ]; then 
 		echo "Pyenv : not installed ... ${version}"
 	else
+		echo "Checking pip for version: $version"
 		if [ ! -e ${PYENV_ROOT}/versions/${version}/bin/pip ]; then
-	        echo "Error: pip is not installed for python versions: ${version}"
 	        echo "Setting up pip for version ${version}"
 	        pyenv local $version
 			wget https://bootstrap.pypa.io/get-pip.py
 			python get-pip.py
 			pip install dotfiles
 			rm get-pip.py
+		else
+			echo "pip is alread installed for python versions: ${version}"
 	    fi 
     fi	
 }
@@ -25,4 +27,6 @@ pyenv_install_pip(){
 # Keep 2.7.14 as default python version
 pyenv_install_pip 2.7.14
 pyenv_install_pip 3.6.3
+
+
 
