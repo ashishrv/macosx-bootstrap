677abed16f66781151c5379e140eddfb2afbd751
Ashish Ranjan Vidyarthi <ashish.vid@gmail.com>
2020-05-20 00:00:12 -0700

    Update setting up pyenv

--- pyenv/setup_pyenv.sh
+++ pyenv/setup_pyenv.sh
@@ -4,6 +4,6 @@ fact=`command -v pyenv`
 if [ "x${fact}" == "x" ]; then
     echo "pyenv is not installed."
     echo "Installing  ... pyenv"
-    brew upgrade 'openssl@1.1'
+    #brew upgrade 'openssl@1.1'
     brew install pyenv
 fi
